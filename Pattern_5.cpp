0475b80518f5a523ebb9c32697b1f5f07274940e
Amisha <amisha2@gmail.com>
2023-02-01 18:46:03 +0530

    DSA

---
+++ Pattern_5.cpp
@@ -0,0 +1,20 @@
+//Pattern
+/*
+   * * * * *
+   * * * * 
+   * * * 
+   * *
+   * 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+void seeding(int n) {
+	// Write your code here.
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<(n-i+1);j++){
+			cout<<"* ";
+		}
+		cout<<endl;
+	}
+}
