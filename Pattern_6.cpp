cb3fc545aaa6cbb18f9f53866b16d00ce78d708c
Amisha <amisha2@gmail.com>
2023-02-02 13:14:47 +0530

    DSA

---
+++ Pattern_6.cpp
@@ -0,0 +1,18 @@
+//Pattern
+/*
+  1 2 3
+  1 2 
+  1
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void nNumberTriangle(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        for(int j=1;j<(n-i+1);j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
