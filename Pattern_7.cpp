a28488964e5b1cb4e3fab23fc9eba3f09c75c6b1
Amisha <amisha2@gmail.com>
2023-02-02 13:37:55 +0530

    DSA

---
+++ Pattern_7.cpp
@@ -0,0 +1,27 @@
+//Pattern
+/*
+      *
+     ***
+    *****
+   *******  
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void nStarTriangle(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<2*i+1;j++){
+            cout<<"*";
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+      cout<<endl;
+    }
+    
+}
