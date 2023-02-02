c732dbf1fe4249da8008bd28ecb5304d7bc7e01d
Amisha Kumari <87603586+Amishaaaaa@users.noreply.github.com>
2023-02-02 13:54:16 +0530

    DSA

--- Pattern_8.cpp
+++ Pattern_8.cpp
@@ -12,12 +12,15 @@ using namespace std;
 void nStarTriangle(int n) {
     // Write your code here.
     for(int i=0;i<n;i++){
+        //Space
         for(int j=0;j<i;j++){
            cout<<" "; 
         }
+        //Star
         for(int j=0;j<(2*n)-(2*i+1);j++){
             cout<<"*";
         }
+         //Space
          for(int j=0;j<i;j++){
            cout<<" "; 
         }
