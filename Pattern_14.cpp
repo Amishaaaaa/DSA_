77668821a7d517c5b9faf15461a9c00af9dcd978
Amisha <amisha2@gmail.com>
2023-02-03 16:40:36 +0530

    DSA

---
+++ Pattern_14.cpp
@@ -0,0 +1,19 @@
+//Pattern
+/* 
+   A
+   A B
+   A B C
+   A B C D
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void nLetterTriangle(int n) {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        for(char ch='A';ch<='A'+i;ch++){
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
