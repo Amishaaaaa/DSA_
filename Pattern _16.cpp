22998c0012d9d8e3970a9676a3f5f1306c05b52a
Amisha <amisha2@gmail.com>
2023-02-06 13:20:21 +0530

    DSA

---
+++ Pattern _16.cpp
@@ -0,0 +1,42 @@
+//Pattern
+/* 
+  A
+  B B
+  C C C
+  D D D D
+  E E E E E 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+char ch='A';
+void alphaRamp(int n) {
+    char ch='A';
+    // Write your code here.
+    for(int i=1;i<=n;i++){
+      for(int j=0;j<i;j++){
+        cout<<ch<<" ";
+        
+      }
+      ch++; 
+      cout<<endl;
+    }
+}
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void alphaRamp(int n) {
+    char ch='A';
+    // Write your code here.
+    for(int i=0;i<n;i++){
+      char ch='A'+i;
+      for(int j=0;j<=i;j++){
+        cout<<ch<<" ";
+        
+      }
+     
+      cout<<endl;
+    }
+}
