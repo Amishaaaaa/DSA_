fa3d31233183818c51a082248a22e315936a698c
Amisha <amisha2@gmail.com>
2023-02-24 18:43:51 +0530

    DSA

---
+++ Recursion_5.cpp
@@ -0,0 +1,17 @@
+//Print from n to 1 using backtracking
+#include<bits/stdc++.h>
+using namespace std;
+
+void funct(int i,int n){
+    if(i>n){
+        return;
+    }
+    
+    funct(i+1,n);
+    cout<<i<<endl;
+}
+int main(){
+    int n;
+    cin>>n;
+    funct(1,n);
+}
