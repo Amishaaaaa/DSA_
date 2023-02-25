c2a8a605375df93e780794763150ca298027b882
Amisha <amisha2@gmail.com>
2023-02-25 14:38:58 +0530

    DSA

---
+++ Recursion_6.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Submission of first n numbers
+int funct(int n){
+    if(n==0){
+        return 0;
+    }
+    return n+funct(n-1);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<funct(n);
+}
