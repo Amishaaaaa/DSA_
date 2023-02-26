12ca4807dad68990ba063fda419f88599db0d6f3
Amisha <amisha2@gmail.com>
2023-02-26 15:41:10 +0530

    DSA

---
+++ Recursion_9.cpp
@@ -0,0 +1,21 @@
+//Palindrome check
+#include<bits/stdc++.h>
+using namespace std;
+
+bool f(int i,string &s,int n){
+    if(i>=n/2){
+        return true;
+    }
+    if(s[i]!=s[n-i-1]){
+        return false;
+    }
+    return f(i+1,s,n);
+}
+
+int main(){
+    string s;
+    cin>>s;
+    int n=s.length();
+    cout<<f(0,s,n);
+
+}
